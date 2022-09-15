788e36cb4fb885c81ad5411c68cac6c589564c27
ssuchichan@gmail.com <ssuchichan@gmail.com>
2022-09-15 15:28:59 +0800

    crud unit tests.

--- Makefile
+++ Makefile
@@ -24,4 +24,8 @@ migratedown1:
 sqlc:
 	sqlc generate
 
-.PHONY: postgres createdb dropdb migrateup migratedown migrateup1 migratedown1 sqlc
+test:
+	go test -v -cover ./...
+
+.PHONY: postgres createdb dropdb migrateup migratedown migrateup1 migratedown1 sqlc test
+
---
+++ db/sqlc/main_test.go
@@ -0,0 +1,26 @@
+package sqlc
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+var testQueries *Queries
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:csq2400306@localhost:5432/bank?sslmode=disable"
+)
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatalln("connot open to db:", err)
+	}
+	testQueries = New(conn)
+	os.Exit(m.Run())
+}
